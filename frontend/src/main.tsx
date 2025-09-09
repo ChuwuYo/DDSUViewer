574bca86ea714019c3a757e75d5c05d50b352183
ChuwuYo <141227996+ChuwuYo@users.noreply.github.com>
2025-09-09 11:13:42 +0800

    chore: 更改配置
    HeroUI to Chakra - UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import {createRoot} from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
-import './style.css'
+import { createRoot } from 'react-dom/client'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import App from './App'
+import './style.css'
 
 const container = document.getElementById('root')
+if (!container) throw new Error('Root container not found')
 
-const root = createRoot(container!)
+const root = createRoot(container)
 
 root.render(
-    <React.StrictMode>
-        <ChakraProvider>
-            <App/>
-        </ChakraProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider value={defaultSystem}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 )
